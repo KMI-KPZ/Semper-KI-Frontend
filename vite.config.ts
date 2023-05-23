ea25819be85f14cd26956a53548c13b6af7e5251
Lucas <lucas.martin@infai.org>
2023-05-23 16:06:04 +0200

    refactor vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   server: {
     open:true,
-    port: 3000
+    port: 3000,
+    host: '127.0.0.1',
   },
   build:{
     outDir: 'build',

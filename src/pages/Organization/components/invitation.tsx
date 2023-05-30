fafe5811ff3c6690105611acf985aafa80a10288
Lucas <lucas.martin@infai.org>
2023-05-30 16:47:46 +0200

    first implementation of yup and react-hook-forms

--- src/pages/Organization/components/invitation.tsx
+++ src/pages/Organization/components/invitation.tsx
@@ -4,6 +4,15 @@ import React, { useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import useOrganizations from "../hooks/useOrganizations";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    email: yup.string().required().email(),
+  })
+  .required();
+type FormData = yup.InferType<typeof schema>;
 
 interface InvitationProps {}
 
@@ -16,17 +25,36 @@ const Invitation: React.FC<InvitationProps> = (props) => {
   const { t } = useTranslation();
   const [showCopy, setShowCopy] = useState<boolean>(false);
   const [showLoadedIn, setshowLoadedIn] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>("");
+  // const [email, setEmail] = useState<string>("");
   const { inviteLinkQuery, inviteUserMutation } = useOrganizations();
 
   const {
+    reset,
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-  console.log(watch("email"));
+  } = useForm<FormData>({ resolver: yupResolver(schema) });
+  const onSubmit = (data: FormData) => {
+    console.log("Submit", data);
+    inviteUserMutation.mutate(data.email, {
+      onSuccess(data, variables, context) {
+        setshowLoadedIn(true);
+        setTimeout(() => {
+          setshowLoadedIn(false);
+        }, 2000);
+        reset();
+      },
+      // onError(error, variables, context) {
+      //   setshowLoadedIn(true);
+      //   setTimeout(() => {
+      //     setshowLoadedIn(false);
+      //   }, 2000);
+      //   reset();
+      // },
+    });
+  };
+  // console.log(watch("email"));
 
   const handleOnClickLink = (
     e: React.MouseEvent<HTMLInputElement, MouseEvent>
@@ -35,7 +63,7 @@ const Invitation: React.FC<InvitationProps> = (props) => {
   };
 
   const handleOnChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+    // setEmail(e.target.value);
   };
 
   const handleOnClickCopy = () => {
@@ -48,46 +76,55 @@ const Invitation: React.FC<InvitationProps> = (props) => {
     }
   };
   const handleOnClickInvite = () => {
-    inviteUserMutation.mutate(email, {
-      onSuccess(data, variables, context) {
-        setshowLoadedIn(true);
-        setTimeout(() => {
-          setshowLoadedIn(false);
-        }, 2000);
-        setEmail("");
-      },
-    });
+    // inviteUserMutation.mutate(email, {
+    //   onSuccess(data, variables, context) {
+    //     setshowLoadedIn(true);
+    //     setTimeout(() => {
+    //       setshowLoadedIn(false);
+    //     }, 2000);
+    //     setEmail("");
+    //   },
+    // });
   };
 
   return (
     <div className="flex w-full flex-col items-center justify-center gap-5">
       <h2>{t("Organization.components.invitation.header")}</h2>
-      <div
+      <form
         className="relative flex w-full flex-col items-center justify-between gap-5 md:w-1/2 md:flex-row"
         onSubmit={handleSubmit(onSubmit)}
       >
-        {/* <input
-          className="w-full bg-slate-100 px-5 py-2"
-          defaultValue="test"
-          {...register("email")}
-        /> */}
         <input
+          className="w-full bg-slate-100 px-5 py-2"
+          placeholder={t("Organization.components.invitation.placeholder")}
+          {...register("email")}
+        />
+        {/* <input
           type="email"
           className="w-full bg-slate-100 px-5 py-2"
           placeholder={t("Organization.components.invitation.placeholder")}
           value={email}
           onChange={handleOnChangeEmail}
-        />
+        /> 
         <Button onClick={handleOnClickInvite}>
           {t("Organization.components.invitation.button.invite")}
         </Button>
+        */}
+        <input
+          type="submit"
+          className="bezier flex w-full flex-row   items-center justify-center  gap-3 bg-türkis px-5 py-3 text-white transition duration-300 hover:cursor-pointer hover:bg-grau-400 md:w-fit md:px-4 md:py-2"
+        />
         {showLoadedIn ? (
           <div className="absolute -right-28 w-fit p-5">
             {t("Organization.components.invitation.button.send")}
-            {errors.email && <span>This field is required</span>}
           </div>
         ) : null}
-      </div>
+        {errors.email !== undefined ? (
+          <div className="absolute -right-60 w-fit p-5">
+            {errors.email.message}
+          </div>
+        ) : null}
+      </form>
       <LoadingSuspense query={inviteLinkQuery}>
         <div className="relative flex w-full flex-col items-center justify-between gap-5 text-center md:w-1/2 md:flex-row">
           <input

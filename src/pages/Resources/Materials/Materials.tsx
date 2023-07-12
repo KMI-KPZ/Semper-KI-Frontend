00d3f81dd969f51319280a69d754a71665d610c3
Lucas <lucas.martin@infai.org>
2023-07-12 12:28:53 +0200

    refactor printers add route

--- src/pages/Resources/Materials/Materials.tsx
+++ src/pages/Resources/Materials/Materials.tsx
@@ -15,10 +15,15 @@ const ResourcesMaterials: React.FC<ResourcesMaterialsProps> = (props) => {
     <div className="flex w-full flex-col items-center justify-start gap-5 bg-white p-3">
       <Routes>
         <Route index element={<ResourcesMaterialsTable />} />
-        <PermissionGate
-          element="ResourcesMaterialsForm"
-          showMessage
-          children={<Route path="add" element={<ResourcesMaterialsForm />} />}
+        <Route
+          path="add"
+          element={
+            <PermissionGate
+              element="ResourcesMaterialsForm"
+              showMessage
+              children={<ResourcesMaterialsForm />}
+            />
+          }
         />
       </Routes>
     </div>

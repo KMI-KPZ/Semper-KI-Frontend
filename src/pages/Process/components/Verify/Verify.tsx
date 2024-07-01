730199298a73f996ac1015156c1916269cce85e7
Lucas Martin <lucas.martin@infai.org>
2024-07-01 12:26:37 +0200

    implemented Verify Status Mock

--- src/pages/Process/components/Verify/Verify.tsx
+++ src/pages/Process/components/Verify/Verify.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from "react-i18next";
 import ProcessStatusButtons from "../StatusButtons";
 import ProcessContainer from "@/components/Process/Container";
 import ProcessHeader from "@/components/Process/Header";
+import ProcessVerifyCard, { VerifyStatus } from "./components/VerifyCard";
 
 interface ProcessVerifyProps {}
 
@@ -18,6 +19,17 @@ const ProcessVerify: React.FC<ProcessVerifyProps> = (props) => {
         menuTitle={t("Process.components.Verify.Verify.button.menu")}
         pageTitle={t("Process.components.Verify.Verify.title")}
       ></ProcessHeader>
+      <Container
+        width="full"
+        direction="row"
+        className="flex-wrap md:flex-nowrap"
+        align="start"
+      >
+        <ProcessVerifyCard status={VerifyStatus.READY} type="PRINTABILITY" />
+        <ProcessVerifyCard status={VerifyStatus.STARTED} type="DRAFT" />
+        <ProcessVerifyCard status={VerifyStatus.COMPLETED} type="CAPACITY" />
+        <ProcessVerifyCard status={VerifyStatus.FAILED} type="STABILITY" />
+      </Container>
       <ProcessStatusButtons
         start={ProcessStatus.CONTRACTOR_SELECTED}
         end={ProcessStatus.VERIFIED}
--- src/pages/Process/components/Verify/components/VerifyCard.tsx
+++ src/pages/Process/components/Verify/components/VerifyCard.tsx
@@ -1,14 +1,39 @@
-import { Container } from "@component-library/index";
+import { Container, Heading, Text } from "@component-library/index";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import ProcessVerifyStatus from "./VerifyStatus";
 
-interface ProcessVerifyCardProps {}
+interface ProcessVerifyCardProps {
+  type: VerifyType;
+  status: VerifyStatus;
+}
+
+export type VerifyType = "PRINTABILITY" | "DRAFT" | "CAPACITY" | "STABILITY";
+export enum VerifyStatus {
+  "READY",
+  "STARTED",
+  "ONGOING",
+  "COMPLETED",
+  "FAILED",
+}
 
 const ProcessVerifyCard: React.FC<ProcessVerifyCardProps> = (props) => {
-  const {} = props;
+  const { status, type } = props;
   const { t } = useTranslation();
 
-  return <Container></Container>;
+  return (
+    <Container direction="col" className="card" width="full" height="full">
+      <Heading variant="h3">
+        {t(`Process.components.Verify.components.VerifyCard.title.${type}`)}
+      </Heading>
+      <ProcessVerifyStatus status={status} />
+      <Text>
+        {t(
+          `Process.components.Verify.components.VerifyCard.description.${type}`
+        )}
+      </Text>
+    </Container>
+  );
 };
 
 export default ProcessVerifyCard;

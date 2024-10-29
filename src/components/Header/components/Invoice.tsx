5e3a5a13cea489e2064ecbef9824d22553e55682
Lucas Martin <lucas.martin@infai.org>
2024-10-29 17:07:14 +0100

    refactor invoice

--- src/components/Header/components/Invoice.tsx
+++ src/components/Header/components/Invoice.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import {
-  Button,
-  Container,
-  Divider,
-  Heading,
-  Text,
-} from "@component-library/index";
+import { Button, Container, Heading, Text } from "@component-library/index";
 import useEvents from "@/hooks/useEvents/useEvents";
 import CloseIcon from "@mui/icons-material/Close";
 import useDeleteEvent from "@/api/Events/Mutations/useDeleteEvent";
@@ -14,6 +8,7 @@ import useDeleteEvents from "@/api/Events/Mutations/useDeleteEvents";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import { Event } from "@/hooks/useEvents/EventTypes";
 import { useNavigate } from "react-router-dom";
+import { ProcessStatus } from "@/api/Process/Querys/useGetProcess";
 
 interface HeaderInvoiceProps {
   closeInvoice: () => void;
@@ -57,15 +52,57 @@ const HeaderInvoice: React.FC<HeaderInvoiceProps> = (props) => {
     }
   };
 
+  const getEventContent = (event: Event): string => {
+    switch (event.eventData.reason) {
+      case "test":
+        return event.eventData.content;
+      case "files":
+        return event.eventData.content;
+      case "messages":
+        return `"` + event.eventData.content + `"`;
+      case "serviceDetails":
+        return event.eventData.content;
+      case "serviceType":
+        return event.eventData.content;
+      case "serviceStatus":
+        return event.eventData.content;
+      case "processDetails":
+        return event.eventData.content;
+      case "processStatus":
+        return t(
+          `enum.ProcessStatus.${
+            ProcessStatus[
+              Number(event.eventData.content)
+            ] as keyof typeof ProcessStatus
+          }`
+        );
+      case "provisionalContractor":
+        return event.eventData.content;
+      case "dependenciesIn":
+        return event.eventData.content;
+      case "dependenciesOut":
+        return event.eventData.content;
+      case "roleChanged":
+        return event.eventData.content;
+      case "userDeleted":
+        return event.eventData.content;
+    }
+  };
+
   return (
     <Container
-      className="card absolute left-0 top-20 z-40 gap-3 bg-white"
+      className="card absolute left-0 top-20 z-40 max-h-[80vh] gap-3 overflow-y-auto bg-white  p-0"
       direction="col"
       justify="start"
       align="center"
       width="fit"
     >
-      <Container width="full" direction="row" justify="between" className="">
+      <Container
+        width="full"
+        direction="row"
+        justify="between"
+        className="sticky top-0 z-50 border-b-2 bg-white p-5"
+      >
         <Heading variant="h2">
           {t("components.Header.HeaderInvoice.title")}
         </Heading>
@@ -77,46 +114,47 @@ const HeaderInvoice: React.FC<HeaderInvoiceProps> = (props) => {
           onClick={closeInvoice}
         />
       </Container>
-      <Divider />
 
-      {events.length === 0 ? (
-        <Text>{t("components.Header.HeaderInvoice.noEvents")}</Text>
-      ) : (
-        <>
-          <Button
-            title={t("components.Header.HeaderInvoice.deleteAll")}
-            startIcon={<DeleteOutlineOutlinedIcon />}
-            size="sm"
-            variant="secondary"
-            className=""
-            onClick={handleOnClickButtonDeleteEvents}
-          />
-          {events.map((event, index) => (
-            <Container
-              key={index}
-              className=" relative gap-2 rounded-md border-2 p-3 duration-300 hover:cursor-pointer hover:border-gray-300 hover:bg-gray-50"
-              width="full"
-              align="start"
-              direction="col"
-              onClick={() => handleOnClickButtonEvent(event)}
-            >
-              <Button
-                title={t("components.Header.HeaderInvoice.delete")}
-                children={<DeleteOutlineOutlinedIcon />}
-                size="sm"
-                variant="text"
-                className="absolute right-1 top-1"
-                onClick={() => handleOnClickButtonDeleteEvent(event.eventID)}
-              />
-              <Heading variant="h3">
-                {t(`types.EventDataReason.${event.eventData.reason}`)}
-              </Heading>
-              <Text>"{event.eventData.content}"</Text>
-              <Text>{event.createdWhen.toLocaleString()}</Text>
-            </Container>
-          ))}
-        </>
-      )}
+      <Container width="full" direction="col" className="gap-3 p-3">
+        {events.length === 0 ? (
+          <Text>{t("components.Header.HeaderInvoice.noEvents")}</Text>
+        ) : (
+          <>
+            <Button
+              title={t("components.Header.HeaderInvoice.deleteAll")}
+              startIcon={<DeleteOutlineOutlinedIcon />}
+              size="sm"
+              variant="secondary"
+              className=""
+              onClick={handleOnClickButtonDeleteEvents}
+            />
+            {events.map((event, index) => (
+              <Container
+                key={index}
+                className=" relative gap-2 rounded-md border-2 p-3 duration-300 hover:cursor-pointer hover:border-gray-300 hover:bg-gray-50"
+                width="full"
+                align="start"
+                direction="col"
+                onClick={() => handleOnClickButtonEvent(event)}
+              >
+                <Button
+                  title={t("components.Header.HeaderInvoice.delete")}
+                  children={<DeleteOutlineOutlinedIcon />}
+                  size="sm"
+                  variant="text"
+                  className="absolute right-1 top-1"
+                  onClick={() => handleOnClickButtonDeleteEvent(event.eventID)}
+                />
+                <Heading variant="h3">
+                  {t(`types.EventDataReason.${event.eventData.reason}`)}
+                </Heading>
+                <Text>{getEventContent(event)}</Text>
+                <Text>{event.createdWhen.toLocaleString()}</Text>
+              </Container>
+            ))}
+          </>
+        )}
+      </Container>
     </Container>
   );
 };

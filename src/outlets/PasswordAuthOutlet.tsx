61b6edb0ac7174c7640b5d9f5d8709f74c055849
Lucas Martin <lucas.martin@infai.org>
2024-11-25 15:09:38 +0100

    refactor encypted pw

--- src/outlets/PasswordAuthOutlet.tsx
+++ src/outlets/PasswordAuthOutlet.tsx
@@ -2,6 +2,7 @@ import React, { PropsWithChildren, useState } from "react";
 import { Button, Heading } from "@component-library/index";
 import { useForm } from "react-hook-form";
 import Cookies from "js-cookie";
+import bcrypt from "bcryptjs";
 
 interface PasswordAuthOutletProps {}
 interface FormData {
@@ -12,28 +13,41 @@ const PasswordAuthOutlet: React.FC<
   PropsWithChildren<PasswordAuthOutletProps>
 > = (props) => {
   const { children } = props;
-  const hiddenPW = "h5XxXR8o9NISyZiZJeiQ";
+
+  // Store the hashed version of the password
+  const hiddenPWHash =
+    "$2a$12$xELjU7VcxqPEScFh8kQ0wOiTGY2DP8blYFQPtCtHeaeVDJYNjeMCa";
   const [userPassword, setUserPassword] = useState<string>(
     Cookies.get("NRU-APP-PASSWORD") || ""
   );
 
-  const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit, reset } = useForm<FormData>();
 
   const onSubmit = (data: FormData) => {
-    Cookies.set("NRU-APP-PASSWORD", data.password);
-    setUserPassword(data.password);
+    // Compare the hash of the input password with the stored hash
+    const isMatch = bcrypt.compareSync(data.password, hiddenPWHash);
+    if (isMatch) {
+      Cookies.set("NRU-APP-PASSWORD", data.password);
+      setUserPassword(data.password);
+    } else {
+      alert("Incorrect password");
+    }
+    reset();
   };
 
-  if (hiddenPW !== userPassword)
+  // Verify password validity
+  const isValidPassword = bcrypt.compareSync(userPassword, hiddenPWHash);
+
+  if (!isValidPassword)
     return (
       <form className="flex h-screen w-screen flex-col items-center justify-center gap-5 bg-white text-black">
-        <Heading variant="h1">Password Authentification</Heading>
+        <Heading variant="h1">Password Authentication</Heading>
         <input
           type="password"
           {...register("password")}
           className="rounded-md border-2 p-2"
         />
-        <Button title="Entsperren" onClick={handleSubmit(onSubmit)} size="sm" />
+        <Button title="Unlock" onClick={handleSubmit(onSubmit)} size="sm" />
       </form>
     );
   return children;

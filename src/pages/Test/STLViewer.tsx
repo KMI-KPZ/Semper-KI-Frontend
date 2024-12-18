3db0ddfe75dbe864d195374c94685178e12d4815
Akshay NS <shewatkar@infai.org>
2024-12-18 15:52:37 +0100

    works, alignment and measurements(axes) nw

--- src/pages/Test/STLViewer.tsx
+++ src/pages/Test/STLViewer.tsx
@@ -1,56 +1,11 @@
-// import { twMerge } from "tailwind-merge";
-// import { StlViewer } from "react-stl-viewer";
-// import { LoadingAnimation } from "@component-library/index";
-
-// const ModelPreview = (props: {
-//   file?: string;
-//   className?: string;
-//   interactive?: boolean;
-// }) => {
-//   const { file, className: _className, interactive = true } = props;
-
-//   const className = twMerge(
-//     "overflow-clip w-full h-full rounded-md border-2 max-h-[600px] max-w-[1000px] h-[300px] w-[500px] ",
-//     _className
-//   );
-
-//   if (file === undefined) return <LoadingAnimation />;
-//   return (
-//     <StlViewer
-//       className={className}
-//       url={file}
-//       orbitControls={interactive}
-//       shadows
-//       showAxes
-//       modelProps={{ color: "red", scale: 1.5 }}
-//     />
-//   );
-//   // return (
-//   //   <div
-//   //     className={twMerge(
-//   //       "h-[600px] w-[1000px] overflow-clip rounded-md border-2",
-//   //       className
-//   //     )}
-//   //   >
-//   //     <Canvas>
-//   //       <CameraControls />
-//   //       <ambientLight intensity={0.7} />
-//   //       <directionalLight position={[10, -40, 30]} intensity={2} />
-//   //       {file !== undefined ? <Model fileURL={file} /> : <Loading />}
-//   //     </Canvas>
-//   //   </div>
-//   // );
-// };
-
-// export default ModelPreview;
-
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { Suspense } from "react";
+import { Suspense, useLayoutEffect } from "react";
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
 import * as THREE from "three";
 import { useLoader } from "@react-three/fiber";
 import { LoadingAnimation } from "@component-library/index";
+import { AxesHelper } from "three";
 
 const STLModel = (props: { file: string }) => {
   const geometry = useLoader(STLLoader, props.file);
@@ -58,14 +13,30 @@ const STLModel = (props: { file: string }) => {
   // Center and scale the geometry
   geometry.computeBoundingBox();
   const center = geometry.boundingBox?.getCenter(new THREE.Vector3());
+  const size = geometry.boundingBox?.getSize(new THREE.Vector3());
+  const maxDimension = Math.max(size?.x || 1, size?.y || 1, size?.z || 1);
+  const scaleFactor = 1 / maxDimension; // Scale to fit within a unit cube
+
   if (center) geometry.center();
+  console.log('center:', center)
+  console.log('size:', size)
 
   return (
-    <mesh geometry={geometry}>
+    <mesh geometry={geometry} scale={[scaleFactor, scaleFactor, scaleFactor]}>
       <meshStandardMaterial color="red" />
     </mesh>
   );
 };
+
+function Upper(props: { file: string }) {
+  const geomUpper = useLoader(STLLoader, props.file)
+  useLayoutEffect(() => {
+    const mesh = new THREE.Mesh(geomUpper);
+    const box = new THREE.Box3();
+    box.expandByObject(mesh);
+  }, [props.file])
+  return <primitive object={geomUpper} />
+}
 
 const ModelPreview = (props: {
   file?: string;
@@ -79,7 +50,7 @@ const ModelPreview = (props: {
   return (
     <div className={className}>
       <Canvas
-        camera={{ position: [5, 5, 5], fov: 75 }}
+        camera={{ position: [10, 10, 10], fov: 75 }}
         style={{
           width: "100%",
           height: "100%",
@@ -91,7 +62,9 @@ const ModelPreview = (props: {
           <ambientLight intensity={0.5} />
           <directionalLight position={[10, 10, 10]} intensity={1} />
           <STLModel file={file} />
+          <Upper file={file} />
           {interactive && <OrbitControls enableDamping />}
+          <primitive object={new AxesHelper(5)} position={[0, 0, 0]} />
         </Suspense>
       </Canvas>
     </div>

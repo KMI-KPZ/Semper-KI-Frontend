b0ad0cbb4a1ebe368464bfbfd8806d9fe5a2c0f0
Lucas Martin <lucas.martin@infai.org>
2025-01-14 11:11:00 +0100

    lil change

--- src/hooks/Process/useScrollIntoView.tsx
+++ src/hooks/Process/useScrollIntoView.tsx
@@ -1,12 +1,28 @@
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import useProcess from "./useProcess";
 interface useScrollIntoViewReturnProps {}
 
 const useScrollIntoView = (newestID?: string): useScrollIntoViewReturnProps => {
   const { hash } = useLocation();
   const navigate = useNavigate();
+  const {} = useProcess();
 
   useEffect(() => {
+    // const elementId = hash.slice(1);
+    // if (elementId) {
+    //   const element = document.getElementById(
+    //     elementId === "newest" && newestID !== undefined ? newestID : elementId
+    //   );
+    //   // logger("useScrollIntoView | useEffect | id", element, newestID);
+    //   if (element) {
+    //     setTimeout(() => {
+    //       element.scrollIntoView({ behavior: "smooth", block: "start" });
+    //       navigate("# ");
+    //       history.replaceState(null, "", " ");
+    //     }, 500);
+    //   }
+    // }
     const elementId = hash.slice(1);
     if (elementId) {
       const element = document.getElementById(

d98f7217243ec37111410582bcba0a053d9c14c9
Lucas Martin <lucas.martin@infai.org>
2025-01-29 11:23:31 +0100

    refactor map

--- src/components/Process/ContractorMap.tsx
+++ src/components/Process/ContractorMap.tsx
@@ -4,11 +4,12 @@ import {
   TileLayer,
   Polyline,
   Marker,
-  Popup,
   useMap,
+  Tooltip,
 } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
+import { LatLngBoundsExpression, LatLngExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { t } from "i18next";
 
 // Typing for the coordinates
 type Coordinates = {
@@ -33,20 +34,12 @@ const ContractorMap: React.FC<ContractorMapProps> = (props) => {
     lng: contractorCoordinates[1],
   };
 
-  // Calculate the midpoint between the two points
-  const calculateMidPoint = (
-    point1: Coordinates,
-    point2: Coordinates
-  ): Coordinates => {
-    const lat = (point1.lat + point2.lat) / 2;
-    const lng = (point1.lng + point2.lng) / 2;
-    return { lat, lng };
-  };
-
-  const midpoint = calculateMidPoint(pointA, pointB);
-
-  // The polyline will connect pointA and pointB
   const lineCoordinates: LatLngExpression[] = [
+    [pointA.lat, pointA.lng],
+    [pointB.lat, pointB.lng],
+  ];
+
+  const bounds: LatLngBoundsExpression = [
     [pointA.lat, pointA.lng],
     [pointB.lat, pointB.lng],
   ];
@@ -60,23 +53,36 @@ const ContractorMap: React.FC<ContractorMapProps> = (props) => {
     return null;
   };
 
+  const FitBoundsComponent = () => {
+    const map = useMap();
+    useEffect(() => {
+      map.fitBounds(bounds, { padding: [40, 50] });
+    }, [map]);
+    return null;
+  };
+
   return (
     <MapContainer
-      center={[midpoint.lat, midpoint.lng]}
-      zoom={13}
-      style={{ width: "200px", height: "200px" }}
+      bounds={bounds}
+      scrollWheelZoom={false}
+      className="h-[200px] w-[300px] overflow-clip rounded-md  shadow-md"
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      <Marker position={[pointA.lat, pointA.lng]}>
-        <Popup>Point A</Popup>
+      <Marker position={pointA}>
+        <Tooltip permanent direction="top" offset={[-14, 15]}>
+          {t("general.you")}
+        </Tooltip>
       </Marker>
-      <Marker position={[pointB.lat, pointB.lng]}>
-        <Popup>Point B</Popup>
+      <Marker position={pointB}>
+        <Tooltip permanent direction="top" offset={[-14, 15]}>
+          {t("general.manufacturer")}
+        </Tooltip>
       </Marker>
 
       <Polyline positions={lineCoordinates} color="blue" />
 
+      <FitBoundsComponent />
       <MapInvalidator />
     </MapContainer>
   );
